0ae05ca163e88acd02c782f2d3df240c62cb088e
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-08-29 22:32:10 +0300

    Create a play/pause toggle button.

--- gui/controls.py
+++ gui/controls.py
@@ -1,18 +1,51 @@
-import tkinter as ttk
+import tkinter as tk
 
 
 class Controls:
     def __init__(self, parent):
         self.parent = parent
+        self.is_music_playing = False
+        self.play_pause_button = None
 
-    def icon_button(self, icon: ttk.PhotoImage, command=lambda: None):
+    def icon_button(self, icon: tk.PhotoImage, command=lambda: None):
         """
         This method creates a button where the background and the border are removed.
             :param icon: tkinter PhotoImage
             :param command: function (optional)
             :return: tkinter button object
         """
-        button = ttk.Button(self.parent, text="It exists", image=icon, command=command)
-        button.config(relief=ttk.FLAT, borderwidth=0)
+        button = tk.Button(self.parent, image=icon, command=command)
+        button.config(relief=tk.FLAT, borderwidth=0)
 
         return button
+
+    def create_toggle_play_pause_button(
+        self, play_icon: tk.PhotoImage, pause_icon: tk.PhotoImage, command=lambda: None
+    ):
+        """
+        This method creates a play/pause button with toggle functionality.
+            :param play_icon: tkinter PhotoImage
+            :param pause_icon: tkinter PhotoImage
+            :param command: function (optional)
+        """
+        self.play_pause_button = self.icon_button(
+            icon=pause_icon, command=lambda: self.toggle_play_pause(command=command)
+        )
+        self.play_icon = play_icon
+        self.pause_icon = pause_icon
+
+        return self.play_pause_button
+
+    def toggle_play_pause(self, command):
+        """
+        This method toggles the play/pause state of the button
+        """
+        if self.is_music_playing:
+            self.play_pause_button.config(image=self.play_icon)
+            self.is_music_playing = False
+
+        else:
+            self.play_pause_button.config(image=self.pause_icon)
+            self.is_music_playing = True
+
+        command()
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,25 +1,31 @@
-import tkinter as ttk
+import tkinter as tk
 import os
 from config.config import Config
 from gui.controls import Controls
+from gui.utils import resized_img
 
 
-class MainWindow(ttk.Tk):
+class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title(Config.APP_NAME)
         self.controls = Controls(self)
-        self.play_icon = None
+
+        self.play_icon = resized_img(os.path.abspath("./assets/icons/play.png"), 64, 64)
+        self.pause_icon = resized_img(
+            os.path.abspath("./assets/icons/pause.png"), 64, 64
+        )
+
         self.create_widgets()
 
     def create_widgets(self):
         """
         Method which creates widgets on the main window of the music player application.
         """
-        self.play_icon = ttk.PhotoImage(file=os.path.abspath("./assets/icons/play.png"))
-        play_button = self.controls.icon_button(icon=self.play_icon)
-
-        play_button.pack()
+        play_pause_button = self.controls.create_toggle_play_pause_button(
+            play_icon=self.play_icon, pause_icon=self.pause_icon
+        )
+        play_pause_button.pack()
 
     def run(self):
         self.mainloop()
